9824c092946d414ad913c0576c5f14c1630ed1fc
MateoAOA <125922660+MateoAOA@users.noreply.github.com>
2023-02-27 16:49:35 -0600

    agregamos los frame

--- tkinther/practica_11.py
+++ tkinther/practica_11.py
@@ -5,5 +5,15 @@ ventana= Tk()
 ventana.title("Ejemplo de 3 Frames")
 ventana.geometry("600x400")
 
+#2.- agregamos los frame
+seccion1= Frame(ventana, bg="white")
+seccion1.pack(expand=True,fill='both')
+
+seccion2= Frame(ventana, bg="blue")
+seccion2.pack(expand=True,fill='both')
+
+seccion3= Frame(ventana, bg="red")
+seccion3.pack(expand=True,fill='both')
+
 #llamamos al main
 ventana.mainloop()
