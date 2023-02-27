dd31f6cf6dada8435afb8cee6a006cbe64caf2b9
MateoAOA <125922660+MateoAOA@users.noreply.github.com>
2023-02-27 16:41:38 -0600

    se creo la carpena de tkinther y el objeto ventana

---
+++ tkinther/practica_11.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Frame
+
+#1.- instanciamos el objeto ventana
+ventana= Tk()
+ventana.title("Ejemplo de 3 Frames")
+ventana.geometry("600x400")
+
+#llamamos al main
+ventana.mainloop()
